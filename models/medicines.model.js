c8be4ddcf55b7ace4ac31660031202efcdea0b18
Haritha Weerathunga <122088594+harithaw@users.noreply.github.com>
2023-02-24 11:11:42 +0530

    updated medicinen model

--- models/medicines.model.js
+++ models/medicines.model.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const medicineSchema = new Schema({
-    mName:{
-        type: String,
-        required:true,
-    },
     mNDC:{
         type: String,
         required:true,
         unique: true,
+    },
+    mName:{
+        type: String,
+        required:true,
     },
 },{timestamps:true});
 
